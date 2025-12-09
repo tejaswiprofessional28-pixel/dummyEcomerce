621a230ecdf3ec4305c77e5623c07e8dff0ebc40
tejaswiprofessional28-pixel <tejaswiprofessional28@gmail.com>
2025-12-09 20:40:58 +0530

    atlas link is added

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import mongoose, { connect } from "mongoose";
 
 const database = async () =>{
     try{
-        await mongoose.connect('mongodb://localhost:27017/dummyProductAdd');
+        await mongoose.connect('mongodb+srv://TejaswiMeshram:project@123@cluster0.dbdbua0.mongodb.net/?appName=Cluster0/dummyProductAdd');
         console.log('database is connected');
     }
     catch(e){
